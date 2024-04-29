1f97d6e553f737fa723bf3fb15cc252610156f00
Alessandro La Chioma <ale.lach@gmail.com>
2024-04-29 16:11:54 -0400

    Update check_ephys_vs_behavior_alignment__script.m

--- check_ephys_vs_behavior_alignment__script.m
+++ check_ephys_vs_behavior_alignment__script.m
@@ -5,7 +5,12 @@ clear
 % folder_root = 'Y:\Users\ariadna\ephys\h-tester-align-data\h-test_mic9_\2024-03-18';
 % folder_root = 'Y:\Users\ariadna\ephys\h-tester-align-data\h-test_ttlcam_rest\2024-03-18';
 % folder_root = 'Y:\Users\ariadna\ephys\h-tester-align-data\h-test_ttlcam\2024-03-18';
-folder_root = 'Z:\Users\Alessandro La Chioma\Ariadna\ale_tmp\trial02';
+% folder_root = 'Z:\Users\Alessandro La Chioma\Ariadna\ale_tmp\trial03';
+% folder_root = 'Z:\Users\Alessandro La Chioma\Ariadna\ale_tmp\smart_mouse\2024-04-25';
+folder_root = 'Z:\Users\Alessandro La Chioma\Ariadna\ale_tmp\smart_mouse\2024-04-26\trial001'; % Misalignment: ev_dt = -2.1617 msec
+% folder_root = 'Z:\Users\Alessandro La Chioma\Ariadna\ale_tmp\smart_mouse\2024-04-26\trial002'; % Misalignment: ev_dt = -2.1308 msec
+% folder_root = 'Z:\Users\Alessandro La Chioma\Ariadna\ale_tmp\smart_mouse\2024-04-26\trial003'; % Misalignment: ev_dt = -2.1931 msec
+
 
 %% Load data.mat file
 
@@ -66,71 +71,130 @@ MicSamps_all = [1 : n_Samps]';
 % clock) corresponding to the audio file samples indicated in MicSamps.
 % % T_all = interp1(MicSamps, MicTimeStamps, MicSamps_all, 'linear', 'extrap');
 
-% % MicSamps = [1; cumsum(MicNrSamples(1:end-1)) + 1];
-% % T_all = interp1(MicSamps, MicTimeStamps, MicSamps_all, 'linear', 'extrap');
+% MicSamps = [1; cumsum(MicNrSamples(1:end-1)) + 1];
+% T_all = interp1(MicSamps, MicTimeStamps, MicSamps_all, 'linear', 'extrap');
 
 
 tCaptureStart = MicTimeStamps(1);
-% T_all = (MicSamps_all-1) / Fs_mic;
-T_all = (MicSamps_all-1) / Fs_mic + tCaptureStart;
+T_all = (MicSamps_all-1) / Fs_mic;
+% T_all = (MicSamps_all-1) / Fs_mic + tCaptureStart;
 
 %% Load TTL ephys and get timestamp 
 
-ttlephys_ls = dir([folder_root filesep '*_TTLephys.flac']);
 
-ttl_ephys.fullpath_flac = fullfile(ttlephys_ls.folder, ttlephys_ls.name);
+%     ttl_ephys_type = 'dedicated';
+    ttl_ephys_type = 'mic';
 
-disp(['Extracting TTL ephys timestamps from audio channel '  '...']) 
+switch ttl_ephys_type
+    case 'dedicated'
 
-[recaudiodata,Fs] = audioread(ttl_ephys.fullpath_flac);
-% Fs = D.audio_rec.ttl_ephys.SampleRate;
-ttl_eph = recaudiodata(:);
-clear recaudiodata
-                    
-val_low  =  D.audio_rec.ttl_ephys.val_low;
-val_high =  D.audio_rec.ttl_ephys.val_high;
-dval_min =  D.audio_rec.ttl_ephys.dval_min;
-val_min  =  val_high;
-                    
-                    
-% Extract timestamps of White Matter Ephys TTLs (in microphone samples)
+        ttlephys_ls = dir([folder_root filesep '*_TTLephys.flac']);
+        
+        ttl_ephys.fullpath_flac = fullfile(ttlephys_ls.folder, ttlephys_ls.name);
+        
+        disp(['Extracting TTL ephys timestamps from audio channel '  '...']) 
 
-min_dist = 1; % Minimum distance of ttl onsets, in Hz
-% Find peak (% locs contains the timestamps of the
-% TTL onsets (in microphone samples))
-[pks,locs_down] = findpeaks(ttl_eph, "MinPeakDistance",Fs/min_dist,...
-    "MinPeakProminence",dval_min, "MinPeakHeight",val_min); 
-nr_detectedPulses = length(locs_down);
-
-if nr_detectedPulses < 1
-    fprintf('Warning: No TTL Ephys pulses found! \n');
-elseif nr_detectedPulses > 1
-    fprintf('!!! Error: Nr. %d TTL Ephys pulses found.\n', nr_detectedPulses);
-else
-    fprintf('TTL Ephys pulse found, good. \n');
-end
-
-MicSamples     = locs_down;
-MicTimeStamps  = D.audio_rec.ttl_ephys.MicTimeStamps;
-MicNrSamples   = D.audio_rec.ttl_ephys.MicNrSamples;
-ttlEphysTimeStamps = nan(nr_detectedPulses,1);
-for f = 1 : nr_detectedPulses
-    samp = locs_down(f); % frame onset in sound card samples
-    ind_MicNrSamples = find( (samp - MicNrSamples)> 0, 1,"first");
-    d_samps = samp - MicNrSamples(ind_MicNrSamples);
-    ttlEphysTimeStamps(f) = MicTimeStamps(ind_MicNrSamples) + d_samps/D.audio_rec.ttl_ephys.SampleRate;
-end
-D.audio_rec.ttl_ephys.TimeStamps = ttlEphysTimeStamps;
-D.audio_rec.ttl_ephys.MicSamples = MicSamples;
+        [recaudiodata,Fs] = audioread(ttl_ephys.fullpath_flac);
+        % Fs = D.audio_rec.ttl_ephys.SampleRate;
+        ttl_eph = recaudiodata(:);
+        clear recaudiodata
+                            
+        val_low  =  D.audio_rec.ttl_ephys.val_low;
+        val_high =  D.audio_rec.ttl_ephys.val_high;
+        dval_min =  D.audio_rec.ttl_ephys.dval_min;
+        val_min  =  val_high;
+                            
+                            
+        % Extract timestamps of White Matter Ephys TTLs (in microphone samples)
+        
+        min_dist = 1; % Minimum distance of ttl onsets, in Hz
+        % Find peak (% locs contains the timestamps of the
+        % TTL onsets (in microphone samples))
+        [pks,locs_down] = findpeaks(ttl_eph, "MinPeakDistance",Fs/min_dist,...
+            "MinPeakProminence",dval_min, "MinPeakHeight",val_min); 
+        nr_detectedPulses = length(locs_down);
+        
+        if nr_detectedPulses < 1
+            fprintf('Warning: No TTL Ephys pulses found! \n');
+        elseif nr_detectedPulses > 1
+            fprintf('!!! Error: Nr. %d TTL Ephys pulses found.\n', nr_detectedPulses);
+        else
+            fprintf('TTL Ephys pulse found, good. \n');
+        end
+        
+        MicSamples     = locs_down;
+        MicTimeStamps  = D.audio_rec.ttl_ephys.MicTimeStamps;
+        MicNrSamples   = D.audio_rec.ttl_ephys.MicNrSamples;
+        %%% If ephys TTL is fed to one of the four mic channels:
+            MicTimeStamps = D.audio_rec.MicTimeStamps;
+            MicNrSamples  = D.audio_rec.MicNrSamples;
+        %%%
+        ttlEphysTimeStamps = nan(nr_detectedPulses,1);
+        for f = 1 : nr_detectedPulses
+            samp = locs_down(f); % frame onset in sound card samples
+            ind_MicNrSamples = find( (samp - MicNrSamples)> 0, 1,"first");
+            d_samps = samp - MicNrSamples(ind_MicNrSamples);
+            ttlEphysTimeStamps(f) = MicTimeStamps(ind_MicNrSamples) + d_samps/D.audio_rec.ttl_ephys.SampleRate;
+        end
+        D.audio_rec.ttl_ephys.TimeStamps = ttlEphysTimeStamps;
+        D.audio_rec.ttl_ephys.MicSamples = MicSamples;
     
-figure
-k = 1000;
-inds_toPlot = MicSamples+[-k:k];
-plot(inds_toPlot, ttl_eph(inds_toPlot))
+    case 'mic'
 
-% Pulse duration (manually enter the sample nr of the offset):
-samp_offset = 33255;
-ttlpulse_dur = abs(samp - samp_offset)/D.audio_rec.ttl_ephys.SampleRate;
+        ttl_eph = y(:,2);
+        
+        figure;plot(ttl_eph)
+        Fs = Fs_mic;
+        val_low  = 0;
+        val_high = max(ttl_eph);
+        dval_min = val_high/2;
+        val_min  =  dval_min;
+        
+        min_dist = 1; % Minimum distance of ttl onsets, in Hz
+        % Find peak (% locs contains the timestamps of the
+        % TTL onsets (in microphone samples))
+        [pks,locs_down] = findpeaks(ttl_eph, "MinPeakDistance",Fs/min_dist,...
+            "MinPeakProminence",dval_min, "MinPeakHeight",val_min); 
+        nr_detectedPulses = length(locs_down);
+        
+        if nr_detectedPulses < 1
+            fprintf('Warning: No TTL Ephys pulses found! \n');
+        elseif nr_detectedPulses > 1
+            fprintf('!!! Error: Nr. %d TTL Ephys pulses found.\n', nr_detectedPulses);
+        else
+            fprintf('TTL Ephys pulse found, good. \n');
+        end
+
+        MicSamples     = locs_down;
+        % MicTimeStamps  = D.audio_rec.ttl_ephys.MicTimeStamps;
+        % MicNrSamples   = D.audio_rec.ttl_ephys.MicNrSamples;
+        %%% If ephys TTL is fed to one of the four mic channels:
+            MicTimeStamps = D.audio_rec.MicTimeStamps;
+            MicNrSamples  = D.audio_rec.MicNrSamples;
+        %%%
+        ttlEphysTimeStamps = nan(nr_detectedPulses,1);
+        for f = 1 : nr_detectedPulses
+            samp = locs_down(f); % frame onset in sound card samples
+            ttlEphysTimeStamps(f) = T_all(samp);
+        end
+
+end
+
+
+figure
+k = Fs/2; % 1000;
+inds_toPlot = MicSamples+[-k:k];
+p = plot(inds_toPlot, ttl_eph(inds_toPlot));
+p.DataTipTemplate.DataTipRows(1).Format = '%d'; % set precision of any datatip you add to 1 unit (by default it could be 5 units)
+
+% Find TTL ephys offset, so to calculate pulse duration:
+[~,locs_down2] = findpeaks(-ttl_eph, "MinPeakDistance",Fs/min_dist,...
+    "MinPeakProminence",dval_min, "MinPeakHeight",val_min); 
+samp_offset = locs_down2(1);
+% % Pulse duration (manually enter the sample nr of the offset):
+% samp_offset = 3843411;
+
+ttlpulse_dur = abs(samp - samp_offset)/Fs;
 fprintf('ttlpulse_dur = %5.4f msec\n\n', ttlpulse_dur*1000);
 
 %% Load ephys data 
@@ -177,30 +241,30 @@ yephys = yephys*6.25e3/32768;
 
 data_ephys = yephys(:,1);
 
-
 sampvec_ephys  = [1 : length(data_ephys)]';
 tvec_ephys     = [sampvec_ephys-1] / Fs_ephys;
 tvec_ephys_ptb = tvec_ephys + ttlEphysTimeStamps;
 
 figure;
 plot(tvec_ephys, data_ephys)
+ylabel('Ephys trace')
+xlabel('Time (s, ephys clock)')
 
 %%
 
 trange_ephys = [26.5 26.57];
-trange_ephys = [296.05 296.1];
+trange_ephys = [0.05 0.1];
+trange_ephys = [56.23 56.25];
+trange_ephys = [59.46 59.49];
+% trange_ephys = [55.25 55.4];
+% trange_ephys = [9.9 10];
 trange_ephys_samp = trange_ephys * Fs_ephys;
 trange_ephys_ptb  = trange_ephys + ttlEphysTimeStamps;
 
 
 %%
 
-figure
-p = plot(sampvec_ephys, data_ephys);
-p.DataTipTemplate.DataTipRows(1).Format = '%d'; % set precision of any datatip you add to 1 unit (by default it could be 5 units)
-ylabel('Ephys trace')
-xlabel('Samples (ephys samps)')
-xlim(trange_ephys_samp)
+
 
 figure
 plot(tvec_ephys, data_ephys)
@@ -214,18 +278,26 @@ ylabel('Ephys trace')
 xlabel('Time (s, ptb clock)')
 xlim(trange_ephys_ptb)
 
+figure
+p = plot(sampvec_ephys, data_ephys);
+p.DataTipTemplate.DataTipRows(1).Format = '%d'; % set precision of any datatip you add to 1 unit (by default it could be 5 units)
+ylabel('Ephys trace')
+xlabel('Samples (ephys samps)')
+xlim(trange_ephys_samp)
+title(sprintf('Use this plot to choose one sample point as you like.'));
+
 fprintf('\nUse the plot with Ephys data as a function of Samples to choose one sample point as you like.\n') 
 fprintf('The same point will be found in the behavior data stream (mic or ttlcam) and used for checking alignment.\n')
 fprintf('Enter the sample number (x value) of this point in the variable ''ev_samp''\n\n');
 
 %%
 
-ev_samp   = 662791;
-ev_samp   = 1563899;
+% ev_samp   = 662791;
+% ev_samp   = 1563899;
 % ev_samp   = 405482;
 % ev_samp   = 139705;
 % ev_samp   = 230350;
-ev_samp   = 7401544;
+ev_samp   = 1486723;
 ev_tephys = tvec_ephys(ev_samp);
 ev_tptb   = tvec_ephys_ptb(ev_samp);
 
@@ -262,19 +334,52 @@ ylabel('Behavior data (mic or ttlcam)')
 xlabel('Samples (samps)')
 xlim(trange_mic_samp)
 p.DataTipTemplate.DataTipRows(1).Format = '%d'; % set precision of any datatip you add to 1 unit (by default it could be 5 units)
+title(sprintf('Use this plot to find the same point you chose before.'));
 
 fprintf('\nUse the plot with Behavior data as a function of Samples to find the same point you chose before.\n') 
 fprintf('Enter the sample number (x value) of this point in the variable ''ev_mic_samp''\n\n');
 
-%%
+%% Get misalignment between ephys and behavior
 
 ev_mic_samp = 7998175;
 ev_mic_samp = 14918705;
-% ev_mic_samp = 4804690;
-ev_mic_samp = 60146464;
+ev_mic_samp = 15242625;
 ev_mic_tptb = T_all(ev_mic_samp);
 
 ev_dt = ev_tptb - ev_mic_tptb;
 fprintf('Misalignment: ev_dt = %5.4f msec\n\n', ev_dt*1000);
 
+
+%% Plot everything together
+
+data_mic = y(:,1);
+ttl_eph  = y(:,2);
+
+MicSamps_all = [1 : n_Samps]';
+tvec_mic = (MicSamps_all-1) / Fs_mic;
+
+% Get range of data_mic to rescale ephys later on:
+lims = prctile(data_mic, [1 99]);
+lims = [-max(abs(lims)) max(abs(lims))];
+
+figure;
+hold on
+plot(tvec_mic, data_mic)
+plot(tvec_mic, ttl_eph)
+
+data_ephys      = yephys(:,1);
+% Rescale ephys data:
+lims_ephys = prctile(data_ephys, [1 99]);
+% lims_ephys = [min(data_ephys), max(data_ephys)];
+data_ephys_norm = (data_ephys - lims_ephys(1)) / (lims_ephys(2)-lims_ephys(1)) * 2 - 1;
+data_ephys_norm = data_ephys_norm * lims(2);
+
+sampvec_ephys  = [1 : length(data_ephys)]';
+tvec_ephys     = [sampvec_ephys-1] / Fs_ephys;
+tvec_ephys2mic = tvec_ephys + ttlEphysTimeStamps;
+
+plot([ttlEphysTimeStamps ttlEphysTimeStamps],[-1 1]*lims(2),'k--')
+plot(tvec_ephys2mic, data_ephys_norm)
+
+ylim(lims)
 
